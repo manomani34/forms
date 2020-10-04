eeb0582b4289cfacba1b317734902c8f47644f17
Name <Email>
2020-10-04 22:13:59 +0330

    home
    home start project!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'forms';
+
+  @ViewChild('f') inputForm: NgForm;
+  defaultGender = 'male';
+  alert = '';
+  defaultRadio = 'dog';
+  animals = ['dog', 'cat', 'horse', 'bee'];
+
+  user= {
+    username: '',
+    email: '',
+    gender: '',
+    alert: '',
+    animal: ''
+  }
+
+  isSubmited = false;
+
+    onClick() {
+    // this.inputForm.setValue({
+    //   groupUser:{
+    //     username: 'ali',
+    //     email: ''
+    //   },
+    //   gender: '',
+    //   alert: '',
+    //   animal: '', 
+    // });
+    this.inputForm.form.patchValue({
+      groupUser: {
+        username: 'ali',
+      }
+      });
+  }
+
+  onSubmit() {
+    this.isSubmited = true;
+    this.user.username = this.inputForm.value.groupUser.username;
+    this.user.email = this.inputForm.value.groupUser.email;
+    this.user.gender = this.inputForm.value.gender;
+    this.user.alert = this.inputForm.value.alert;
+    this.user.animal = this.inputForm.value.animal;
+
+    this.inputForm.reset();
+  }
 }
